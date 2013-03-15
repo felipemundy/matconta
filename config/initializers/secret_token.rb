73672bc3b0401ff73944822e462a12a569c24f83
Felipe Condé <felipemundy@gmail.com>
2013-03-15 19:11:06 -0300

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Matconta::Application.config.secret_token = '5ea378947337b9e12bc97ebfc0286d2d8ae82f4eae32bf013dfbb4348b58542f81632629e81f3070bcac8e64044faf7bf2d184745a0e9b709ab9c15cd534f4e7'
